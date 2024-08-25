518ac90f2b8b9300a1830da02404d8ef36f2c69f
Elton Santos <eltonaxl@hotmail.com>
2024-08-25 14:24:06 -0300

    First executed page

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Index", nil)
 }
